f404558b94e72f9a950afd8277d447bca434bea2
Raihanul Islam Refat <refat0167@gmail.com>
2020-05-13 12:14:18 +0600

    Big moment: added React frontend

---
+++ public/precache-manifest.1fc80909dfb39477c8472877b7384523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0109bca4d85748b41c352c1c265a90d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "047e5669564a3908f736",
+    "url": "./static/css/main.58bb7b88.chunk.css"
+  },
+  {
+    "revision": "81383395c8f4b104850c",
+    "url": "./static/js/2.c55c2304.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "./static/js/2.c55c2304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047e5669564a3908f736",
+    "url": "./static/js/main.a15b80d7.chunk.js"
+  },
+  {
+    "revision": "4c237daabbcaea4daf8d",
+    "url": "./static/js/runtime-main.a97b7eec.js"
+  }
+]);
