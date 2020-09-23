dcb723987d3b7b673f821edeabcd3939492919bd
Raihanul Islam Refat <refat0167@gmail.com>
2020-09-24 01:55:07 +0600

    Added frontend build to public folder ready to be used

---
+++ public/precache-manifest.40ee32a705780d9af5a48b6284ba8985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d094eedb87b216eebe69d73628c6fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95a4dbb6a42e3f10f668",
+    "url": "./static/css/main.4c38e246.chunk.css"
+  },
+  {
+    "revision": "55bec274b5b81f00ce9a",
+    "url": "./static/js/2.005c4b5a.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "./static/js/2.005c4b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a4dbb6a42e3f10f668",
+    "url": "./static/js/main.8d3643f3.chunk.js"
+  },
+  {
+    "revision": "3718c7a37cef17fc08ff",
+    "url": "./static/js/runtime-main.23864a07.js"
+  },
+  {
+    "revision": "1b86e225c50a4eb9a7fe5e416902316c",
+    "url": "./static/media/change_cell.1b86e225.mp3"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a4d0bffbe82c6807c9bf1af3512d8a96.js"
+  "./precache-manifest.40ee32a705780d9af5a48b6284ba8985.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
